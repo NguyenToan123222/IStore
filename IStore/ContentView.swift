29fdab1c5ee1a43931aaa8f4f12eb0b2106040c2
NguyenToan123222 <nguyenphutoan25102004@gmail.com>
2025-08-19 14:38:38 +0700

    Initial Commit

---
+++ IStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IStore
+//
+//  Created by Nguyễn Toàn on 19/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IStore/IStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  IStoreApp.swift
+//  IStore
+//
+//  Created by Nguyễn Toàn on 19/8/25.
+//
+
+import SwiftUI
+
+@main
+struct IStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
